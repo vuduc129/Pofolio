5eb89335f20f3311390d594712a88ee13ea0d713
Vu Duc <vuanhduc1209@gmail.com>
2023-04-07 12:55:46 +0700

    Validate

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,118 +1,118 @@
 
-// const Validator = (options)=>{
+const Validator = (options)=>{
 
-//     const ruleSelector={}
-//     const validate=(rule,inputElement)=>{
-//         const errorElement = inputElement.closest(options.formGroup).querySelector(options.errorSelector)
-//         let errorMessage
-//         const rules = ruleSelector[rule.selector]
+    const ruleSelector={}
+    const validate=(rule,inputElement)=>{
+        const errorElement = inputElement.closest(options.formGroup).querySelector(options.errorSelector)
+        let errorMessage
+        const rules = ruleSelector[rule.selector]
         
-//         for(let i = 0;i<rules.length;i++){
-//             errorMessage = rules[i](inputElement.value)
-//             if(errorMessage) break;
-//         }
+        for(let i = 0;i<rules.length;i++){
+            errorMessage = rules[i](inputElement.value)
+            if(errorMessage) break;
+        }
         
-//         if(errorMessage){
-//             errorElement.innerText=errorMessage
-//         }else{
-//             errorElement.innerText=''
-//         }
+        if(errorMessage){
+            errorElement.innerText=errorMessage
+        }else{
+            errorElement.innerText=''
+        }
 
-//         return !errorMessage
-//     }
+        return !errorMessage
+    }
 
-//     const formElement = document.querySelector(options.form)
-//     if(formElement){
+    const formElement = document.querySelector(options.form)
+    if(formElement){
 
-//         formElement.onsubmit = function(e){
+        formElement.onsubmit = function(e){
 
 
-//             let formIsValid = true
+            let formIsValid = true
 
-//             options.rules.forEach(rule=>{
-//                 const inputElement = formElement.querySelector(rule.selector)
-//                 let formValid = validate(rule,inputElement)
-//                 if(!formValid){
-//                     return formIsValid = false
-//                 }
-//             })
+            options.rules.forEach(rule=>{
+                const inputElement = formElement.querySelector(rule.selector)
+                let formValid = validate(rule,inputElement)
+                if(!formValid){
+                    return formIsValid = false
+                }
+            })
 
-//             if(formIsValid){
-//                 let data = {}
-//                 if(typeof options.onSubmit === 'function'){
-//                     const inputs = formElement.querySelectorAll(options.input)
-//                     Array.from(inputs).map(input=>{
-//                         return data[input.id]= input.value
-//                     })
-//                 }
-//                 options.onSubmit(data)
+            if(formIsValid){
+                let data = {}
+                if(typeof options.onSubmit === 'function'){
+                    const inputs = formElement.querySelectorAll(options.input)
+                    Array.from(inputs).map(input=>{
+                        return data[input.id]= input.value
+                    })
+                }
+                options.onSubmit(data)
 
-//             }else{
-//                 e.preventDefault()
-//             }
+            }else{
+                e.preventDefault()
+            }
     
-//         }
+        }
 
-//         options.rules.forEach(rule=>{
+        options.rules.forEach(rule=>{
 
-//             if(Array.isArray(ruleSelector[rule.selector])){
-//                 ruleSelector[rule.selector].push(rule.func)
-//             }else{
-//                 ruleSelector[rule.selector] = [rule.func]
-//             }
+            if(Array.isArray(ruleSelector[rule.selector])){
+                ruleSelector[rule.selector].push(rule.func)
+            }else{
+                ruleSelector[rule.selector] = [rule.func]
+            }
 
-//             const inputElement = formElement.querySelector(rule.selector)
-//             const errorElement = inputElement.closest(options.formGroup).querySelector(options.errorSelector)
+            const inputElement = formElement.querySelector(rule.selector)
+            const errorElement = inputElement.closest(options.formGroup).querySelector(options.errorSelector)
 
-//             if(inputElement){
-//                 inputElement.onblur=function(){
-//                     validate(rule,inputElement)
-//                 }
-//                 inputElement.oninput=function(){
-//                     errorElement.innerText=''
-//                 }
-//             }
+            if(inputElement){
+                inputElement.onblur=function(){
+                    validate(rule,inputElement)
+                }
+                inputElement.oninput=function(){
+                    errorElement.innerText=''
+                }
+            }
             
-//         })
-//     }
+        })
+    }
 
-// }
+}
 
-// Validator.isRequired=(selector,message)=>{
-//     return{
-//         selector,
-//         func: (value)=>{
-//             return value ? undefined : message || 'Please fill this form before send email' 
-//         }
-//     }
-// }
+Validator.isRequired=(selector,message)=>{
+    return{
+        selector,
+        func: (value)=>{
+            return value ? undefined : message || 'Please fill this form before send email' 
+        }
+    }
+}
 
-// Validator.isEmail = (selector)=>{
-//     return{
-//         selector,
-//         func:(value)=>{
-//             const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-//             return regex.test(value) ? undefined : 'This form must be an valid email'
-//         }
-//     }
-// }
+Validator.isEmail = (selector)=>{
+    return{
+        selector,
+        func:(value)=>{
+            const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            return regex.test(value) ? undefined : 'This form must be an valid email'
+        }
+    }
+}
 
-// Validator.isNumber = (selector)=>{
-//     return{
-//         selector,
-//         func:(value)=>{
-//             const regex= /^[0-9]*$/
-//             return regex.test(value) ? undefined : 'This form requires number content only'
-//         }
-//     }
-// }
+Validator.isNumber = (selector)=>{
+    return{
+        selector,
+        func:(value)=>{
+            const regex= /^[0-9]*$/
+            return regex.test(value) ? undefined : 'This form requires number content only'
+        }
+    }
+}
 
-// Validator.lengthRequired = (selector,length)=>{
-//     return{
-//         selector,
-//         func:(value)=>{
-//             return value.length > length ? undefined : `This form must be longer than ${length} characters!`
-//         }
-//     }
-// }
+Validator.lengthRequired = (selector,length)=>{
+    return{
+        selector,
+        func:(value)=>{
+            return value.length > length ? undefined : `This form must be longer than ${length} characters!`
+        }
+    }
+}
 
